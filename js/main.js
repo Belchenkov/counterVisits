6aaf0d49542dbce2476f6650e0f7dbef889a8124
Aleksey Belchenkov <u608110@gmail.com>
2017-04-18 18:38:38 +0300

    Fixed Get IP and type field date on DATETIME

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var domain = 'busines-credit.ru';
+    var domain = (window.location.hostname);
     var ip = '';
 
     // Get IP
@@ -10,55 +10,42 @@ $(document).ready(function() {
             ip = location;
             console.log(location);
 
-            $.ajax({
-                url: 'https://api.filkos.com/getBidData.php',
-                type: 'GET',
-                success: function (data) {
-                    var data = JSON.parse(data);
-                    data = data.sources;
-                    $.each(data, function (index, item) {
-                        //console.log(item.domain);
-                        //console.log(domain);
-                        if (domain == item.domain) {
-                            console.log('Isset match ', item.domain);
+            // Check on new User
+            if (!$.cookie(ip)) {
+                // Set Date
+                var date = new Date();
+                var currentDate = date.getUTCFullYear() + '-' + (date.getUTCMonth()+1) + '-' + date.getUTCDate() + ' ' + date.getUTCHours() + ':' + date.getUTCMinutes() + ":" + date.getUTCSeconds();
 
-                            // Check if new User
-                            if (!$.cookie(ip)) {
-                                // Set Date
-                                var date = new Date();
-                                var currentDate = date.getUTCFullYear() + '-' + (date.getUTCMonth()+1) + '-' + date.getUTCDate() + ' ' + date.getUTCHours() + ':' + date.getUTCMinutes() + ":" + date.getUTCSeconds();
+                //For Minutes
+                //var minutes = 0.2;
+                //date.setTime(date.getTime() + (minutes * 60 * 1000));
+                //$.cookie(ip, currentDate, { expires: date });
 
-                                // One Day
-                                $.cookie(ip, currentDate, { expires: 1 });
-                                console.log('Новый посетитель!', currentDate);
+                // One Day
+                $.cookie(ip, currentDate, { expires: 1 });
+                console.log('Новый посетитель!', currentDate);
 
 
-                                // Send on DateBase
-                                $.ajax({
-                                    url: 'server/count.php',
-                                    type: 'POST',
-                                    data: {
-                                        domain: domain,
-                                        ip: ip,
-                                        date: currentDate
-                                    },
-                                    success: function(date) {
-                                        console.log(date);
-                                    },
-                                    error: function(error) {
-                                        console.log(error);
-                                    }
-                                });
-                            } else {
-                                console.log('Такой посетитель уже сегодня заходил!');
-                            }
-                        }
-                    });
-                },
-                error: function(error) {
-                    console.log(error);
-                }
-            });
+                // Send on DateBase
+                $.ajax({
+                    url: 'server/count.php',
+                    type: 'POST',
+                    data: {
+                        domain: domain,
+                        ip: ip,
+                        date: currentDate
+                    },
+                    success: function(date) {
+                        console.log(date);
+                    },
+                    error: function(error) {
+                        console.log(error);
+                    }
+                });
+            } else {
+                console.log('Такой посетитель уже сегодня заходил!');
+            }
         }
     }); // end ajax()
+
 });
